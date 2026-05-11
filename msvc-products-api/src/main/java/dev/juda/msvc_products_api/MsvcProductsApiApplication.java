3bf8dae609c5f1acdb9baa54ca2131e27349ca7d
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-05-11 11:44:44 -0500

    Add ReplyInbox bean to application context

--- msvc-products-api/src/main/java/dev/juda/msvc_products_api/MsvcProductsApiApplication.java
+++ msvc-products-api/src/main/java/dev/juda/msvc_products_api/MsvcProductsApiApplication.java
@@ -1,13 +1,19 @@
 package dev.juda.msvc_products_api;
 
+import dev.juda.libs_msvc_commons.domain.messaging.ReplyInbox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MsvcProductsApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsvcProductsApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsvcProductsApiApplication.class, args);
+    }
 
+    @Bean
+    public ReplyInbox inbox() {
+        return new ReplyInbox();
+    }
 }
