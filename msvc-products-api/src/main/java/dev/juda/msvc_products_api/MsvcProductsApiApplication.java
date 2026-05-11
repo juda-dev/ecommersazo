dbe73d4ae93c87b485b06f38e2dc2ab52779dc09
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-05-11 10:39:53 -0500

    Initialize Spring Boot project for msvc-products-api

---
+++ msvc-products-api/src/main/java/dev/juda/msvc_products_api/MsvcProductsApiApplication.java
@@ -0,0 +1,13 @@
+package dev.juda.msvc_products_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcProductsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcProductsApiApplication.class, args);
+	}
+
+}
