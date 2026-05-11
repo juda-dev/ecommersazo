ab9efdb9b809c71e795fef3b614d678d43f04d04
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-05-11 17:13:44 -0500

    Add Maven wrapper and initial project scaffold for msvc-products-query

---
+++ msvc-products-query/src/main/java/dev/juda/msvc_products_query/MsvcProductsQueryApplication.java
@@ -0,0 +1,13 @@
+package dev.juda.msvc_products_query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcProductsQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcProductsQueryApplication.class, args);
+	}
+
+}
