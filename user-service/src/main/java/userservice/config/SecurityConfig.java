ffd26fee9dfdf2a3ea0a93ce35d4b985027db02d
JuDa Dev <judadev@proton.me>
2026-06-11 01:05:31 -0500

    chore(user-service): remove unused ReactiveJwtAuthenticationConverterAdapter import
    - Clean up import that is no longer needed in SecurityConfig

--- user-service/src/main/java/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/userservice/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
-import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
